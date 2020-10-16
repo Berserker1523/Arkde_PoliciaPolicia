58abb7e76e59fe4bbf05af5522f19400b84200c9
Jesús Orlando Cárcamo Posada <berserker1523@hotmail.com>
2020-10-16 17:57:35 -0500

    Unreal project created

---
+++ Source/PoliciaPolicia/PoliciaPoliciaGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PoliciaPoliciaGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class POLICIAPOLICIA_API APoliciaPoliciaGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
